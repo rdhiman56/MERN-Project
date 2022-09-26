01d6e9d290e6e2f709e48d58a1b1cee83a1417ca
Rahul Dhiman <rdhiman56@gmail.com>
2022-09-26 21:22:18 +0530

    "message": "Route is working fine!" in post man  http://localhost:4000/api/v1/products

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getAllProduct } = require("../controllers/productController");
 
 const router = express.Router();
-router.route("products").get(getAllProduct);
+router.route("/products").get(getAllProduct);
 
 module.exports = router;

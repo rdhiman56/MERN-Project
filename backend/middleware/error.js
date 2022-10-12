1a773dd51607005b660de23d5ac2136e501e7e69
Rahul Dhiman <rdhiman56@gmail.com>
2022-10-12 12:38:06 +0530

    bed-userpass-auth

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,18 +1,36 @@
-const ErrorHandler = require('../utils/errorhandler');
+const ErrorHandler = require("../utils/errorhandler");
 
 module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error!";
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error!";
 
-    // wrong mongoDB id Error
-    if(err.name === "CastError"){
-        const message = `Resource Not Found!. Invalid: ${err.path}`;
-        err = new ErrorHandler(message, 400);
-    }
+  // wrong mongoDB id Error
+  if (err.name === "CastError") {
+    const message = `Resource Not Found!. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
 
-    res.status(err.statusCode).json({
-        success: false,
-        //message: err.stack // check fulll detail type error
-        message: err.message
-    });
-}
+  // Mongoose duplicate key errors
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT EXPIRE error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is Expired, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    //message: err.stack // check fulll detail type error
+    message: err.message,
+  });
+};
